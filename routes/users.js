2a0bc5fea899d4c24e30c749867e45419db97315
A-ahmed-younes <a.amkassou@etu.enset-media.ac.ma>
2022-06-01 02:20:28 +0100

    added routes

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,11 @@ const users = require('../data/users.json')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log(req.query)
-  const {skip, take} = req.query
   const u = [...users]
-
+  let {skip,take}=req.query
+  skip=skip|| 0
+  take=take||10
   
-
   res.send(u.splice(skip, take));
 
 });
@@ -19,6 +19,21 @@ router.get('/:id', function(req, res, next) {
   const user = users.find((u)=> u.id === parseInt(req.params.id))
   const r = user ? user : 'not found'
   res.send(r);
-});
+})
+
+router.delete('/:id' , (req,res)=> {
+  console.log (req.params.id);
+  res.send('the user has been deleted');
+})
+
+router.post ('/' , (req,res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
+
+router.patch('/' , (req,res) => {
+  console.log(req.body)
+  res.send(req.body)
+})
 
 module.exports = router;
