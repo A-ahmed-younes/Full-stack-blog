24ce7a096146755f99b1226a3fbe35bcaa864e42
A-ahmed-younes <a.amkassou@etu.enset-media.ac.ma>
2022-06-05 23:47:01 +0100

    stuff

--- routes/users.js
+++ routes/users.js
@@ -14,23 +14,23 @@ router.get('/', function(req, res, next) {
   res.send(u.splice(skip, take));
 
 });
-
+/* GET user by id */
 router.get('/:id', function(req, res, next) {
   const user = users.find((u)=> u.id === parseInt(req.params.id))
   const r = user ? user : 'not found'
   res.send(r);
 })
-
+/* delete user by id */
 router.delete('/:id' , (req,res)=> {
   console.log (req.params.id);
   res.send('the user has been deleted');
 })
-
+/* post user */
 router.post ('/' , (req,res) => {
   console.log(req.body)
   res.send(req.body)
 })
-
+/* patch user */
 router.patch('/' , (req,res) => {
   console.log(req.body)
   res.send(req.body)
