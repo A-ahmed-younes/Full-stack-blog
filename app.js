95f099aa430c0b2f0761678a223a658e906fa2f1
A-ahmed-younes <a.amkassou@etu.enset-media.ac.ma>
2022-06-06 23:43:39 +0100

    auth and frontend

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 var articlesRouter = require('./routes/articles');
 var categoriesRouter = require('./routes/categories');
 var commentairesRouter = require('./routes/commentaires');
+var authRouter = require('./routes/auth');
+
 
 var app = express();
 
@@ -27,6 +29,8 @@ app.use('/users', usersRouter);
 app.use('/articles', articlesRouter);
 app.use('/categories', categoriesRouter);
 app.use('/commentaires', commentairesRouter);
+app.use('/auth', authRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const express = require('express');
+const router = express.Router();
+const auth = require('./auth');
+const createError = require('http-errors')
+router.get('/', (req, res) => {
+    res.send('Hello World!');
 });
-
+router.use('/auth', auth);
+router.use( async (req, res, next) => {
+    next(createError.NotFound('Route not Found'))
+})
+router.use( (err, req, res, next) => {
+    res.status(err.status || 500).json({
+        status: false,
+        message: err.message
+    })
+})
 module.exports = router;
